a13dee76b8583b6072dd0ec03b74a95949074fcb
MAC <hengngounhak@gmail.com>
2021-10-01 09:06:47 +0700

    first commit

--- controllers/url.js
+++ controllers/url.js
@@ -4,55 +4,55 @@ const cheerio = require('cheerio');
 
 exports.newUrl = async(req, res) => {
     if (req.body.liveurl) {
-        await Url.find({ liveurl: req.body.liveurl }).then(async(value) => {
-            if (value.length > 0) {
-                req.session.urlId = value[0]._id;
-                res.send('exist');
-            } else {
-                await axios.get(req.body.liveurl).then(async response => {
-                    const ch = await cheerio.load(await response.data);
-                    const urldata = await ch('script').first().next().html();
-                    if (urldata) {
-                        const livedata = JSON.parse(urldata);
+        // await Url.find({ liveurl: req.body.liveurl }).then(async(value) => {
+        //     if (value.length > 0) {
+        //         req.session.urlId = value[0]._id;
+        //         res.send('exist');
+        //     } else {
+        await axios.get(req.body.liveurl).then(async response => {
+                const ch = await cheerio.load(await response.data);
+                const urldata = await ch('script').first().next().html();
+                if (urldata) {
+                    const livedata = JSON.parse(urldata);
+                    try {
+                        const datetime = new Date(new Date(livedata.dateCreated).toLocaleString("en-US", { timeZone: "Asia/Jakarta" }).toString());
+
+                        const date = (Number(datetime.getDate()) < 10 ? "0" + datetime.getDate() : datetime.getDate()) + "/" +
+                            ((Number(datetime.getMonth()) + 1) < 10 ? "0" + (Number(datetime.getMonth()) + 1) : (Number(datetime.getMonth()) + 1)) +
+                            "/" + datetime.getFullYear();
+                        const time = (datetime.getHours() < 10 ? ("0" + datetime.getHours()) : datetime.getHours()) + ":" + (
+                            datetime.getMinutes() < 10 ? ("0" + datetime.getMinutes()) : datetime.getMinutes());
+                        const myurl = new Url({
+                            pageId: req.session.pageId,
+                            liveurl: req.body.liveurl,
+                            date: date,
+                            time: time
+                        })
+
                         try {
-                            const datetime = new Date(new Date(livedata.dateCreated).toLocaleString("en-US", { timeZone: "Asia/Jakarta" }).toString());
-
-                            const date = (Number(datetime.getDate()) < 10 ? "0" + datetime.getDate() : datetime.getDate()) + "/" +
-                                ((Number(datetime.getMonth()) + 1) < 10 ? "0" + (Number(datetime.getMonth()) + 1) : (Number(datetime.getMonth()) + 1)) +
-                                "/" + datetime.getFullYear();
-                            const time = (datetime.getHours() < 10 ? ("0" + datetime.getHours()) : datetime.getHours()) + ":" + (
-                                datetime.getMinutes() < 10 ? ("0" + datetime.getMinutes()) : datetime.getMinutes());
-                            const myurl = new Url({
-                                pageId: req.session.pageId,
-                                liveurl: req.body.liveurl,
-                                date: date,
-                                time: time
+                            const result = await myurl.save().then((value) => {
+                                req.session.urlId = value._id
                             })
-
-                            try {
-                                const result = await myurl.save().then((value) => {
-                                    req.session.urlId = value._id
+                            if (result) {
+                                res.send({
+                                    commentCount: livedata.commentCount,
+                                    mobileurl: livedata.url
                                 })
-                                if (result) {
-                                    res.send({
-                                        commentCount: livedata.commentCount,
-                                        mobileurl: livedata.url
-                                    })
-                                }
-
-                            } catch (error) {
-                                res.send(false)
                             }
 
                         } catch (error) {
-                            res.send(false);
+                            res.send(false)
                         }
+
+                    } catch (error) {
+                        res.send(false);
                     }
-                }).catch(() => {
-                    res.send(false);
-                })
-            }
-        })
+                }
+            }).catch(() => {
+                res.send(false);
+            })
+            //     }
+            // })
     }
 }
 
